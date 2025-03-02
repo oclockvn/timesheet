ad5ad083b7c39c2a7dd95cb16e7256977f49107d
Quang Phan <oclockvn@gmail.com>
2025-03-02 22:10:56 +0700

    setup migration

--- TimesheetCli/TimesheetCli.Cli/DesignTimeApplicationDbContext.cs
+++ TimesheetCli/TimesheetCli.Cli/DesignTimeApplicationDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using TimesheetCli.Core.Db;
 
 namespace TimesheetCli.Cli;
@@ -13,9 +9,36 @@ public class DesignTimeApplicationDbContext : IDesignTimeDbContextFactory<Applic
 {
     public ApplicationDbContext CreateDbContext(string[] args)
     {
-        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
-            ;
-        //optionsBuilder.bu
-        throw new NotImplementedException();
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddUserSecrets<ApplicationDbContext>()
+            .Build();
+
+        var connectionString = GetConnectionStringFromArgs(args);
+        if (string.IsNullOrWhiteSpace(connectionString))
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new Exception("Connection string is not provided");
+
+        var dbOption = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+        return new ApplicationDbContext(dbOption);
+    }
+
+    private static string GetConnectionStringFromArgs(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "--connection" && i + 1 < args.Length)
+            {
+                return args[i + 1];
+            }
+        }
+
+        return string.Empty;
     }
 }
