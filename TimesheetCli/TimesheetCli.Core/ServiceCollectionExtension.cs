f728f7592aad4a5f0fccb2a1748076a368bc53c2
Quang Phan <oclockvn@gmail.com>
2025-03-02 18:43:56 +0700

    configure db

---
+++ TimesheetCli/TimesheetCli.Core/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using TimesheetCli.Core.Db;
+
+namespace TimesheetCli.Core;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddTimesheetCliCore(this IServiceCollection services, IConfiguration configuration, bool isDevelopment = false)
+    {
+        services.AddApplicationDbContext(configuration, isDevelopment);
+
+        return services;
+    }
+
+    private static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration, bool isDevelopment = false)
+    {
+        services.Configure<DbOption>(x => x.ConnectionString = configuration.GetConnectionString("DefaultConnection")!);
+        services.AddDbContextFactory<ApplicationDbContext>((sp, options) =>
+        {
+            var connectionString = sp.GetRequiredService<IOptions<DbOption>>().Value.ConnectionString;
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Connection string is required");
+            }
+
+            options.UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging(isDevelopment)
+            .EnableDetailedErrors(true)
+            ;
+        }, ServiceLifetime.Scoped);
+
+        return services;
+    }
+}
