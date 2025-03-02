f3d7537a927546ef5ff32887cd4440c514efcfad
Quang Phan <oclockvn@gmail.com>
2025-03-02 18:48:57 +0700

    add design time dbcontext

---
+++ TimesheetCli/TimesheetCli.Cli/DesignTimeApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimesheetCli.Core.Db;
+
+namespace TimesheetCli.Cli;
+
+public class DesignTimeApplicationDbContext : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+            ;
+        //optionsBuilder.bu
+        throw new NotImplementedException();
+    }
+}
