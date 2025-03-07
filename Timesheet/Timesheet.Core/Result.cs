938b8fc6f7303b3e3b88dbac7069b09095622acf
Quang Phan <oclockvn@gmail.com>
2025-03-07 21:46:01 +0700

    add result

---
+++ Timesheet/Timesheet.Core/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace Timesheet.Core;
+
+public class Result<T>
+{
+    public T? Value { get; }
+    public bool IsSuccess => Code == ResultValues.Success;
+    public ResultValues Code { get; }
+
+    public Result(T? value)
+    {
+        Value = value;
+        Code = ResultValues.Success;
+    }
+
+    private Result(ResultValues code)
+    {
+        Code = code;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(ResultValues code) => new(code);
+}
+
+public enum ResultValues
+{
+    Success = 0,
+    TaskNotFound = 1,
+    ProjectNotFound = 2,
+    TaskCodeNotFound = 3,
+    InvalidInput = 4,
+    UnauthorizedAccess = 5,
+    DatabaseError = 6,
+    ValidationError = 7,
+}
